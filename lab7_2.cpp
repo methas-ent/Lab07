853073eb816ba1c4ae40ce85b14a5d14fe15270a
Methas <methas_n@cmu.ac.th>
2025-12-15 21:48:50 +0700

    "lab7_2.cpp completed"

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,45 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string name,movie;
+    string otp = ": ";
+    string ID,Day,text;
+
+    cout<<"Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout<<"?????: ";
+    getline(cin,name);
+    cout<<"Fahsai: Wow!!! "<<name<<" is a really cool name.\n";
+    cout<<"Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    
+    name += otp;
+    cout<<name;
+    cin>> ID;
+    int GEAR  = (ID[0]-'0')*10 + (ID[1] - '0');
+    GEAR -=12;
+    cout<<"Fahsai: I think you may be GEAR "<< GEAR <<". I have a free movie ticket for you.\n";
+    cin.ignore();
+
+    cout<<"Fahsai: Let's go to the cinema together!!!\n";
+    cout<<"Fahsai: What movie do you want to watch?\n";
+
+    cout<<name;
+    getline(cin,movie);
+
+    cout<<"Fahsai: So....which day are you free to go with me?\n";
+    cout<<name;
+    getline(cin,Day);
+
+    cout<<"Fahsai: "<< Day <<"....that is OK!!! I'm looking forward to watching "<<movie<< " with you.\n";
+    cout<<name;
+    cin>>text;
+
+    cout<<"Fahsai: 555+ see you "<< Day <<". Bye Bye \\(^ ^)/" ;
+
+}
